f05083b2b86dfa940a4a97f8d0586e5b65560ce0
fariszain <168964396+fariszain@users.noreply.github.com>
2024-11-28 00:13:11 +0700

    Update Keranjang.java

--- models/Keranjang.java
+++ models/Keranjang.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Keranjang {
-    private List<Barang> barang = new ArrayList<>();
+    private List<Barang> barang = new ArrayList<>();  // Menyimpan daftar barang yang ada di keranjang
 
-public void tambahBarang(Barang barang) {
-    this.barang.add(barang);
-}
+    // Metode untuk menambahkan barang ke dalam keranjang
+    public void tambahBarang(Barang barang) {
+        this.barang.add(barang); 
+    }
 
-public void kosongkanKeranjang() {
-    barang.clear();
-}
+    // Metode untuk mengosongkan keranjang
+    public void kosongkanKeranjang() {
+        barang.clear();
+    }
 
-public List<Barang> getBarang() {
-    return barang;
+    // Metode untuk mendapatkan daftar barang dalam keranjang
+    public List<Barang> getBarang() {
+        return barang;
     }
 }
