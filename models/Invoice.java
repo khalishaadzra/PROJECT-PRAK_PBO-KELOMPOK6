99c4703ec8fb4c3356133ea01667a9ccb016806d
Khalisha Adzraini Arif <166109055+khalishaadzra@users.noreply.github.com>
2024-11-27 23:09:17 +0700

    Update Invoice.java

--- models/Invoice.java
+++ models/Invoice.java
@@ -10,4 +10,20 @@ class Invoice {
         this.transaksi = transaksi;
         this.pembayaran = pembayaran;
     }
+    private String formatHeader() {
+        return "============= INVOICE =============\n";
+    }
+
+    private String formatFooter() {
+        return "===================================\n";
+    }
+
+    private String formatTotalHarga(List<Barang> barangList) {
+        double totalHarga = 0;
+        for (Barang barang : barangList) {
+            totalHarga += barang.getHarga() * barang.getStok(); // Harga x Jumlah Barang
+        }
+        return String.format("Total Harga   : Rp %.2f\n", totalHarga);
+    }
+
 }
