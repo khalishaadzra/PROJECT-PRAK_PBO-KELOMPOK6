629cf5b3ab56d48ba54880510577084641957a3f
fariszain <168964396+fariszain@users.noreply.github.com>
2024-11-27 22:10:27 +0700

    Update Admin.java

---
+++ models/Admin.java
@@ -0,0 +1,26 @@
+package models;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Admin extends Akun {
+    private Akun akun;
+    private List<Barang> listBarang = new ArrayList<>();
+    private List<Transaksi> listTransaksi = new ArrayList<>();
+    public Admin(String id, String password) {
+        super(id, password);
+    }
+
+    public Akun getAkun() {
+        return akun;
+    }
+
+}
