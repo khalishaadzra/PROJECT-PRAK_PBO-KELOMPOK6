ca40bf72caa2a70bd7d76a75e3377677cabf36b0
Khalisha Adzraini Arif <166109055+khalishaadzra@users.noreply.github.com>
2024-11-27 23:11:25 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
 
+import models.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static List<Admin> adminList = new ArrayList<>();
+    private static List<Customer> customerList = new ArrayList<>();
+    private static final String ADMIN_FILE = "admin_data.txt";
+    private static final String CUSTOMER_FILE = "customer_data.txt";
+
+}
