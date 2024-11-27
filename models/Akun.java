609f733edd4d4ef3a29115f566f632da2a5c00b2
nadia <166017227+nadiaamgdln@users.noreply.github.com>
2024-11-27 22:25:36 +0700

    Update Akun.java

--- models/Akun.java
+++ models/Akun.java
@@ -3,4 +3,10 @@ package models;
 public class Akun {
     private String id;
     private String password;
+
+    public Akun(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+    
 }
