ab8d0673e6faf3f9d8eb959779831be48fd41695
Khalisha Adzraini Arif <166109055+khalishaadzra@users.noreply.github.com>
2024-11-28 00:39:35 +0700

    Update Invoice.java

--- models/Invoice.java
+++ models/Invoice.java
@@ -32,27 +32,6 @@ class Invoice {
         return String.format("Total Harga   : Rp %.2f\n", totalHarga); 
     }
 
-    // Metode untuk mencetak invoice ke konsol
-    public void cetakInvoice() {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(formatHeader()); 
-        sb.append("Customer ID   : ").append(transaksi.getCustomer().getId()).append("\n"); 
-        sb.append("Daftar Barang : ");
-        // Menampilkan daftar barang yang dibeli
-        for (Barang barang : transaksi.getBarang()) {
-            sb.append(" ").append(barang.getNama()).append(": Rp ")
-              .append(String.format("%.2f", barang.getHarga())).append("\n");
-        }
-        sb.append("---------------------------------\n");
-        sb.append(formatTotalHarga(transaksi.getBarang())); 
-        sb.append("Metode Bayar  : ").append(pembayaran.getMetodePembayaran()).append("\n"); 
-        sb.append("Pembayaran berhasil diproses.\n"); 
-        sb.append(formatFooter()); 
-
-        System.out.println(sb); 
-    }
-
     // Override toString untuk mengembalikan format invoice sebagai string
     @Override
     public String toString() {
