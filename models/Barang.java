6edc8fdf43d3ac89b05ec6ddd2ffc2a03769587b
nadia <166017227+nadiaamgdln@users.noreply.github.com>
2024-11-28 00:00:36 +0700

    Update Barang.java

--- models/Barang.java
+++ models/Barang.java
@@ -1,36 +1,44 @@
 package models;
 
+// Inisialisasi class Barang
 public class Barang {
     private String nama;
     private double harga;
     private int stok;
 
+    // Konstruktor untuk inisialisasi objek Barang dengan nama, harga, dan stok
     public Barang(String nama, double harga, int stok) {
         this.nama = nama;
         this.harga = harga;
         this.stok = stok;
     }
 
+    // Getter untuk mendapatkan nama barang
     public String getNama() {
         return nama;
     }
 
+    // Setter untuk mengubah nama barang
     public void setNama(String nama) {
         this.nama = nama; 
     }
 
+    // Getter untuk mendapatkan harga barang
     public double getHarga() {
         return harga;
     }
 
+    // Setter untuk mengubah harga barang
     public void setHarga(double harga) {
         this.harga = harga; 
     }
 
+    // Getter untuk mendapatkan jumlah stok barang
     public int getStok() {
         return stok;
     }
 
+    // Setter untuk mengubah jumlah stok barang
     public void setStok(int stok) {
         this.stok = stok;
     }
