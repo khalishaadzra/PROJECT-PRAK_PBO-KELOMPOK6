eebcdc4b71949ff0eb78c438247c876e6e0bda66
nadia <166017227+nadiaamgdln@users.noreply.github.com>
2024-11-27 22:59:32 +0700

    Update Barang.java

--- models/Barang.java
+++ models/Barang.java
@@ -1,1 +1,15 @@
+package models;
+
+public class Barang {
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Barang(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+}
 
