425316a7b1ab8ddbdab737eea49b07f271776f75
fariszain <168964396+fariszain@users.noreply.github.com>
2024-11-28 00:12:15 +0700

    Update Akun.java

--- models/Akun.java
+++ models/Akun.java
@@ -4,16 +4,19 @@ public class Akun {
     private String id;
     private String password;
 
+    // Konstruktor untuk inisialisasi id dan password akun
     public Akun(String id, String password) {
         this.id = id;
-        this.password = password;
+        this.password = password; 
     }
 
+    // Getter untuk mendapatkan id akun
     public String getId() {
         return id;
     }
 
+    // Getter untuk mendapatkan password akun
     public String getPassword() {
-        return password;
-    }    
+        return password; 
+    }
 }
