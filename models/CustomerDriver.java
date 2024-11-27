57fb3d718c59afba02bebd36d80dd4dfe3200191
nadia <166017227+nadiaamgdln@users.noreply.github.com>
2024-11-28 00:26:20 +0700

    Update CustomerDriver.java

--- models/CustomerDriver.java
+++ models/CustomerDriver.java
@@ -3,7 +3,6 @@ package models;
 import java.io.*;
 import java.util.*;
 
-
 // Menginisialisasi class CustomerDriver yang meng-extends class Driver
 public class CustomerDriver extends Driver {
     private Customer customer;
